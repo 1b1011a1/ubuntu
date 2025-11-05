5d74cfd236c82295ec4be27f4d3fdf8248b0e18e
1b1011a1 <m13786942808@163.com>
2025-11-05 21:22:21 +0800

    Init

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "权限不足"
+    exit 1
+fi
+apt update -y
+apt install build-essential -y
+
+if command -v make &> /dev/null; then
+    echo "make 版本：$(make --version | head -n1)"
+else
+    exit 1
+fi
+
