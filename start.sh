14acbd22934ec2a27a7384217615c8cfc61ba44d
1b1011a1 <m13786942808@163.com>
2026-06-29 21:26:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,51 +1,44 @@
 #!/bin/bash
 
+
+
+PUBKEY_FILE="./id_unified.pub"
+ROOT_SSH="/root/.ssh"
+SSHD_CFG="/etc/ssh/sshd_config"
+
+
 if [ "$(id -u)" -ne 0 ]; then
     echo "权限不足 $(id -u)"
     exec sudo bash "$0" "$@"
 fi
-apt update -y
-apt install build-essential -y
 
-if command -v make &> /dev/null; then
-    echo "make 版本：$(make --version | head -n1)"
-else
-    exit 1
-fi
-if command -v tar &> /dev/null; then
-    tar -xjf ssh.tar.bz2
-else
-	apt install tar bzip2 -y
-    if command -v tar &> /dev/null; then
-	    tar -xjf ssh.tar.bz2
-    else
-	    echo "无法解压"
-	    exit 1
-    fi
-fi
-echo "编译sshd"
-cd dropbear-2025.88
-./configure &> /dev/null
-make &> /dev/null
-make install
-if ! command -v /usr/local/sbin/dropbear &> /dev/null; then
-	echo "好像安装了个寂寞"
-    exit 1
-fi
-cd ..
+apt update -y
+apt install openssh-server -y
+
+mkdir -p "$ROOT_SSH"
+chmod 700 "$ROOT_SSH"
+PUB_CONTENT=$(cat "$PUBKEY_FILE")
+echo "$PUB_CONTENT" >> "$ROOT_SSH/authorized_keys"
+chmod 600 "$ROOT_SSH/authorized_keys"
+chown root:root "$ROOT_SSH/authorized_keys"
+
+sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' "$SSHD_CFG"
+sed -i 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' "$SSHD_CFG"
+
+sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' "$SSHD_CFG"
+sed -i 's/^#PasswordAuthentication no/PasswordAuthentication no/' "$SSHD_CFG"
+
+sed -i "s/^PermitRootLogin.*/PermitRootLogin yes/" "$SSHD_CFG"
+
+sed -i 's/^PermitEmptyPasswords yes/PermitEmptyPasswords no/' "$SSHD_CFG"
+sed -i 's/^#PermitEmptyPasswords no/PermitEmptyPasswords no/' "$SSHD_CFG"
+
+sshd -t
+echo "配置通过"
+
 cp ./cpolar /bin/
 chmod 777 /bin/cpolar
 /bin/cpolar authtoken $1
-PUBLIC_KEY="id_unified.pub"
-AUTH_FILE="/root/.ssh/authorized_keys"
-KEY_PATH="/root/.ssh"
-mkdir -p $KEY_PATH
-cp $PUBLIC_KEY $KEY_PATH/
-cat $PUBLIC_KEY > $AUTH_FILE
-chmod 700 $KEY_PATH && chmod 600 $AUTH_FILE
-CONFIG_PATH="/etc/dropbear/dropbear.conf"
-[ -f "/etc/default/dropbear" ] && CONFIG_PATH="/etc/default/dropbear"
-ls /etc/
-ls /etc/default
-grep -q "DropbearAuthorizedKeysFile" $CONFIG_PATH || echo "DropbearAuthorizedKeysFile $AUTH_FILE" >> $CONFIG_PATH
-/usr/local/sbin/dropbear -R &
+
+systemctl enable --now ssh
+systemctl restart ssh
